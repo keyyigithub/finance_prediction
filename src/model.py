b8c5033d871f3bc01557dac7c9b9f400fb7c6133
Jason <2023748942@qq.com>
2025-12-31 20:55:33 +0800

    Adds dropout and attention layer

--- src/model.py
+++ src/model.py
@@ -71,10 +71,10 @@ def build_base_model(input_shape):
     # x = LayerNormalization()(x)
 
     x = LSTM(128, return_sequences=True)(x)
-    # x = Dropout(0.3)(x)
+    x = Dropout(0.3)(x)
     short_cut = x
-    # attention_output_1 = MultiHeadAttention(num_heads=1, key_dim=64)(x, x)
-    # x = LayerNormalization()(x + attention_output_1)
+    attention_output_1 = MultiHeadAttention(num_heads=1, key_dim=64)(x, x)
+    x = LayerNormalization()(x + attention_output_1)
     # attention_output_2 = MultiHeadAttention(num_heads=4, key_dim=256)(x, x)
     # x = LayerNormalization()(x + attention_output_2)
 
