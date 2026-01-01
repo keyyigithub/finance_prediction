a9a1eb731a4251e9291029f27aeebed5842d1e1c
Jason <2023748942@qq.com>
2026-01-01 11:53:07 +0800

    More dropout.

--- src/model.py
+++ src/model.py
@@ -10,7 +10,9 @@ from tensorflow.keras.layers import (
     Conv1D,
     LayerNormalization,
     Concatenate,
-    MultiHeadAttention,
+    MaxPooling1D,
+    SpatialDropout1D,
+    Attention,
     Flatten,
     Bidirectional,
 )
@@ -41,12 +43,12 @@ def build_lstm_residual_block(input_shape, units=256):
 
     shortcut = inputs
 
-    x = Bidirectional(LSTM(units, return_sequences=True, kernel_regularizer=l2(0.01)))(
-        inputs
-    )
-    x = Bidirectional(LSTM(units, return_sequences=True, kernel_regularizer=l2(0.01)))(
-        inputs
-    )
+    x = Bidirectional(
+        LSTM(units, return_sequences=True, kernel_regularizer=l2(0.01), dropout=0.3)
+    )(inputs)
+    x = Bidirectional(
+        LSTM(units, return_sequences=True, kernel_regularizer=l2(0.01), dropout=0.3)
+    )(inputs)
     shortcut_reshaped = Dense(units * 2)(shortcut)
     x = shortcut_reshaped + x
     outputs = Dropout(0.3)(x)
@@ -60,7 +62,8 @@ def build_base_model(input_shape):
     inputs = keras.Input(input_shape)
     x = build_conv_residual_block(input_shape)(inputs)
     x = LayerNormalization()(x)
-    x = Dropout(0.3)(x)
+    x = SpatialDropout1D(0.3)(x)
+    x = MaxPooling1D(pool_size=3)(x)
     # x = build_conv_residual_block(input_shape)(inputs)
     # x = LayerNormalization()(x)
     # x = Dropout(0.3)(x)
@@ -72,13 +75,15 @@ def build_base_model(input_shape):
 
     x = LSTM(128, return_sequences=True)(x)
     short_cut = x
-    attention_output_1 = MultiHeadAttention(num_heads=4, key_dim=128)(x, x)
+    attention_output_1 = Attention(dropout=0.3)(x, x)
     x = LayerNormalization()(x + attention_output_1)
     # attention_output_2 = MultiHeadAttention(num_heads=4, key_dim=256)(x, x)
     # x = LayerNormalization()(x + attention_output_2)
 
     x = Dense(128, activation="relu", kernel_regularizer=l2(0.01))(x)
+    x = Dropout(0.3)(x)
     x = Dense(64, activation="relu", kernel_regularizer=l2(0.01))(x)
+    x = Dropout(0.3)(x)
     short_cut = Dense(64)(short_cut)
     x = short_cut + x
     x = Dropout(0.3)(x)
